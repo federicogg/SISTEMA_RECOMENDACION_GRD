f789cb93f793ae7e73ce6b77849f9e9a7c3da5d8
federicogg <fedeinformatica9@gmail.com>
2020-12-10 14:23:08 +0100

    commit

---
+++ Movie.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self,movieId,title,genres):
+        self.movieId = movieId
+        self.title = title
+        self.genres = genres
+        
--- sistema_recomendacion.py
+++ sistema_recomendacion.py
@@ -1,19 +1,35 @@
 from Rating import Rating 
-
-archivoEntrada = 'ratings.csv'
-
-
-with open(archivoEntrada) as f:
-    ratings = list (f.readlines())
+from Movie import Movie
 
 
+archivoEntradaRating = 'ratings.csv'
+archivoEntradaMovies = 'movies.csv'
 
-for ratingLine in ratings:
+
+with open(archivoEntradaRating) as f1:
+    ratingCSV = list (f1.readlines())
+
+with open(archivoEntradaMovies,encoding="utf8") as f2:
+    moviesCSV = list (f2.readlines())
+
+ratings = []
+movies = []
+
+for ratingLine in ratingCSV:
     results = ratingLine.split(',')
-    print (results[0])
-    
-    
+    ratings.append(Rating(results[0],results[1],results[2],results[3]))
+
+for moviesLine in moviesCSV:
+    results = moviesLine.split(',')
+    movies.append(Movie(results[0],results[1],results[2].rstrip("\n")))
 
 
+userMovies = [
+             {'title':'Toy Story',   'rating':8},
+             {'title':'Seven',       'rating':5},
+             {'title':'Taxi Driver', 'rating':9},
+             {'title':'Rising Sun',  'rating':6},
+             {'title':'Glory',       'rating':4}
+             ]
 
-
+print(userMovies[0])
